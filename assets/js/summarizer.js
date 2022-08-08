25bed82d4d088f14d631a34b91773a2eb719dfd9
Ade <me@shycoder.com>
2022-08-08 15:51:22 +0100

    Cleaned up app

--- assets/js/summarizer.js
+++ assets/js/summarizer.js
@@ -1,168 +1,159 @@
-/**
- * Deals with generating the summary of an article.
- */
+require("dotenv").config({ path: '../../.env' });
 
-// Form
-const title = document.querySelector('input[name=title]');
-const articleForm = document.querySelector('textarea[name=article]');
-const articleFormContainer = document.querySelector('.article-form')
-const articleSummaryContainer = document.querySelector('.article-summary-container');
-const articleTitle = document.querySelector('.article-title');
-const articleSummary = document.querySelector('.article-summary');
-const summarySentenceLength = document.querySelector('input[name=sentence_length]');
+console.log(process.env.API_KEY);
+
+// Form fields
+const title = document.querySelector("input[name=title]");
+const articleForm = document.querySelector("textarea[name=article]");
+const articleFormContainer = document.querySelector(".article-form");
+const articleSummaryContainer = document.querySelector(
+	".article-summary-container"
+);
+const articleTitle = document.querySelector(".article-title");
+const articleSummary = document.querySelector(".article-summary");
+const summarySentenceLength = document.querySelector(
+	"input[name=sentence_length]"
+);
 
 // Buttons
-const summaryButton = document.querySelector('button[name=summarize]');
-const analyzeButton = document.querySelector('button[name=analyze]');
-const resetButton = document.querySelector('button[name=reset]');
-const previousButton = document.querySelector('button[name=previous]');
-const printButton = document.querySelector('button[name=print]');
+const summaryButton = document.querySelector("button[name=summarize]");
+const analyzeButton = document.querySelector("button[name=analyze]");
+const resetButton = document.querySelector("button[name=reset]");
+const previousButton = document.querySelector("button[name=previous]");
+const printButton = document.querySelector("button[name=print]");
 
-let errorMessage = document.querySelector('.error-message');
+let errorMessage = document.querySelector(".error-message");
 
 // Set default input values and state
-const persistedArticle = JSON.parse(localStorage.getItem('article'));
+const persistedArticle = JSON.parse(localStorage.getItem("article"));
 
 if (persistedArticle != null) {
+	if (persistedArticle.text !== null || persistArticleData.text !== "") {
+		summarySentenceLength.value = persistedArticle.length;
+		articleForm.value = persistedArticle.text;
 
-    if (persistedArticle.text !== null || persistArticleData.text !== '') {
-
-        summarySentenceLength.value = persistedArticle.length;
-        articleForm.value = persistedArticle.text;
-
-        analyzeButton.classList.remove('btn-disabled');
-        analyzeButton.disabled = false;
-    }
+		analyzeButton.classList.remove("btn-disabled");
+		analyzeButton.disabled = false;
+	}
 }
 
-articleForm.addEventListener('input', enableAnalyzeButton);
+articleForm.addEventListener("input", enableAnalyzeButton);
 
 function enableAnalyzeButton() {
-    if (this.value !== '') {
+	if (this.value !== "") {
+		analyzeButton.classList.remove("btn-disabled");
+		analyzeButton.disabled = false;
 
-        analyzeButton.classList.remove('btn-disabled');
-        analyzeButton.disabled = false;
-
-        // hide any existing error message
-        errorMessage.style.display = 'none';
-
-    } else {
-        analyzeButton.classList.add('btn-disabled');
-        analyzeButton.disabled = true;
-    }
+		// hide any existing error message
+		errorMessage.style.display = "none";
+	} else {
+		analyzeButton.classList.add("btn-disabled");
+		analyzeButton.disabled = true;
+	}
 }
 
-/**
- * Article summarization logic
- */
-summaryButton.addEventListener('click', summarizeArticle);
+summaryButton.addEventListener("click", summarizeArticle);
 
 function summarizeArticle(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    let text = articleForm.value;
-    let apiKey = '1a8816f5601d3fd6216a8f2c6406d2c6';
+	let text = articleForm.value;
+	let apiKey = process.env.API_KEY;
 
-    generateSummary(text, apiKey);
+	generateSummary(text, apiKey);
 }
 
 function generateSummary(text, apiKey) {
+	if (text !== null && text !== "") {
+		errorMessage.style.display = "none";
+		let sentenceLength = 2;
 
-    if (text !== null && text !== '') {
+		if (summarySentenceLength.value !== "") {
+			if (summarySentenceLength.value.match(/\d/g)) {
+				if (
+					summarySentenceLength.value > 5 ||
+					summarySentenceLength.value < 1
+				) {
+					errorMessage.innerHTML = "Sentences length should not be more than 5";
+					errorMessage.style.display = "block";
+				} else {
+					errorMessage.style.display = "none";
+					sentenceLength = summarySentenceLength.value;
 
-        errorMessage.style.display = 'none';
-        let sentenceLength = 2;
+					persistArticleData();
 
-        if (summarySentenceLength.value !== '') {
+					let xhttp = new XMLHttpRequest();
+					let params = encodeURI(
+						`?key=${apiKey}&txt=${text}&sentences=${sentenceLength}`
+					);
 
-            if (summarySentenceLength.value.match(/\d/g)) {
+					xhttp.onreadystatechange = function () {
+						if (this.readyState == 4 && this.status == 200) {
+							let summary = JSON.parse(this.responseText);
+							displayArticleSummary(summary.summary);
+						}
+					};
 
-                if (summarySentenceLength.value > 5) {
-
-                    errorMessage.innerHTML = 'Sentences length should not be more than 5';
-                    errorMessage.style.display = 'block';
-                } else {
-
-                    errorMessage.style.display = 'none';
-                    sentenceLength = summarySentenceLength.value;
-
-                    persistArticleData();
-
-                    let xhttp = new XMLHttpRequest();
-                    let params = encodeURI(`?key=${apiKey}&txt=${text}&sentences=${sentenceLength}`);
-
-                    xhttp.onreadystatechange = function() {
-                        if (this.readyState == 4 && this.status == 200) {
-
-                            let summary = JSON.parse(this.responseText);
-                            displayArticleSummary(summary.summary);
-                        }
-                    };
-
-                    xhttp.open('POST', 'https://api.meaningcloud.com/summarization-1.0' + params, true);
-                    xhttp.send();
-                }
-            }
-        }
-    } else {
-        errorMessage.innerHTML = 'Article cannot be empty';
-        errorMessage.style.display = 'block';
-    }
+					xhttp.open(
+						"POST",
+						"https://api.meaningcloud.com/summarization-1.0" + params,
+						true
+					);
+					xhttp.send();
+				}
+			}
+		}
+	} else {
+		errorMessage.innerHTML = "Article cannot be empty";
+		errorMessage.style.display = "block";
+	}
 }
 
 function displayArticleSummary(summary) {
-    articleFormContainer.style.display = 'none';
-    articleSummaryContainer.style.display = 'block';
-    articleTitle.value = title;
-    articleSummary.innerHTML = summary;
+	articleFormContainer.style.display = "none";
+	articleSummaryContainer.style.display = "block";
+	articleTitle.value = title;
+	articleSummary.innerHTML = summary;
 }
 
-analyzeButton.addEventListener('click', goToAnalysisPage);
+analyzeButton.addEventListener("click", goToAnalysisPage);
 
 function goToAnalysisPage() {
-    persistArticleData();
-    window.location.href = 'analysis.html';
+	persistArticleData();
+	window.location.href = "analysis.html";
 }
 
 function persistArticleData() {
-    let article = articleForm.value;
-    let articleData = {
-        length: summarySentenceLength.value,
-        text: article
-    }
+	let article = articleForm.value;
+	let articleData = {
+		length: summarySentenceLength.value,
+		text: article,
+	};
 
-    localStorage.setItem('article', JSON.stringify(articleData));
+	localStorage.setItem("article", JSON.stringify(articleData));
 }
 
-resetButton.addEventListener('click', resetForm);
+resetButton.addEventListener("click", resetForm);
 
 function resetForm(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    summarySentenceLength.value = '';
-    articleForm.value = '';
-    analyzeButton.classList.add('btn-disabled');
-    analyzeButton.disabled = true;
-    localStorage.clear();
+	summarySentenceLength.value = "";
+	articleForm.value = "";
+	analyzeButton.classList.add("btn-disabled");
+	analyzeButton.disabled = true;
+	localStorage.clear();
 }
 
-previousButton.addEventListener('click', displayArticleForm);
+previousButton.addEventListener("click", displayArticleForm);
 
 function displayArticleForm() {
-    articleFormContainer.style.display = 'block';
-    articleSummaryContainer.style.display = 'none';
+	articleFormContainer.style.display = "block";
+	articleSummaryContainer.style.display = "none";
 }
 
-printButton.addEventListener('click', printSummary);
+printButton.addEventListener("click", printSummary);
 
 function printSummary() {
-    // let navButtons = document.querySelector('#summaryNavButtons');
-    // let appTitle = document.querySelector('#appTitle');
-    // let appTagline = document.querySelector('#appTagline');
-
-    // navButtons.style.display = 'none';
-    // appTitle.style.display = 'none';
-    // appTagline.style.display = 'none';
-
-    window.print();
+	window.print();
 }
